80ad545f4842d4cb5dc0865ec70165868f039e60
tmba <ut42.nu@gmail.com>
2025-08-07 16:13:06 +0900

    AnimatedWomanコンポーネントを追加し、SocketManagerを実装してキャラクターの状態を管理

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,17 +1,51 @@
-"use client";
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  useCursor,
+} from "@react-three/drei";
 
-import { AnimatedWoman } from "@/components/AnimatedWoman";
-import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
-
+import { useAtom } from "jotai";
+import { useState } from "react";
+import * as THREE from "three";
+import { AnimatedWoman } from "./AnimatedWoman";
+import { charactersAtom, socket } from "./SocketManager";
 export const Experience = () => {
+  const [characters] = useAtom(charactersAtom);
+  const [onFloor, setOnFloor] = useState(false);
+  useCursor(onFloor);
+
   return (
     <>
       <Environment preset="sunset" />
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.3} />
       <ContactShadows blur={2} />
       <OrbitControls />
-      <AnimatedWoman />
-      <AnimatedWoman position-x={1} />
+      <mesh
+        rotation-x={-Math.PI / 2}
+        position-y={-0.001}
+        onClick={(e) => socket.emit("move", [e.point.x, 0, e.point.z])}
+        onPointerEnter={() => setOnFloor(true)}
+        onPointerLeave={() => setOnFloor(false)}
+      >
+        <planeGeometry args={[10, 10]} />
+        <meshStandardMaterial color="#f0f0f0" />
+      </mesh>
+      {characters.map((character: any) => (
+        <AnimatedWoman
+          key={character.id}
+          position={
+            new THREE.Vector3(
+              character.position[0],
+              character.position[1],
+              character.position[2]
+            )
+          }
+          hairColor={character.hairColor}
+          topColor={character.topColor}
+          bottomColor={character.bottomColor}
+        />
+      ))}
     </>
   );
 };
