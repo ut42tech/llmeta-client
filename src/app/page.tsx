a901c5e5d0a1e4b43d1582a47c802535f595c106
tmba <ut42.nu@gmail.com>
2025-08-07 15:41:32 +0900

    SocketManagerコンポーネントをHomeコンポーネントに追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { Experience } from "@/components/Experience";
+import { SocketManager } from "@/components/SocketManager";
 import { Canvas } from "@react-three/fiber";
-
 export default function Home() {
   return (
     <div className="h-screen w-screen">
+      <SocketManager />
       <Canvas shadows camera={{ position: [8, 8, 8], fov: 30 }}>
         <color attach={"background"} args={["#ececec"]} />
         <Experience />
