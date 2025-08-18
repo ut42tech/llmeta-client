f1020c44a8876c38d976abdaaef4970a2b4eba3d
tmba <ut42.nu@gmail.com>
2025-08-18 14:52:25 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import SceneWrapper from "@/components/ExperienceColyseus";
-import { ColyseusClient } from "@/components/ColyseusClient";
-
 export default function Home() {
   return (
     <div className="h-screen w-screen">
-      <ColyseusClient />
-      <SceneWrapper />
+      <DemoSc />
     </div>
   );
 }
