479691ae88fffd1872329474108873aebc7290db
tmba <ut42.nu@gmail.com>
2025-08-18 15:10:02 +0900

    built level

--- src/components/DemoScene.tsx
+++ src/components/DemoScene.tsx
@@ -1,4 +1,5 @@
 import { Sky } from "@react-three/drei";
+import { BvhPhysicsBody, PrototypeBox } from "@react-three/viverse";
 
 export const DemoScene = () => {
   return (
@@ -6,9 +7,41 @@ export const DemoScene = () => {
       {/* Environment */}
       <Sky />
 
-      {/* Basic lighting */}
+      {/* Lighting - expanded with shadow settings */}
       <directionalLight intensity={1.2} position={[5, 10, 10]} castShadow />
       <ambientLight intensity={1} />
+
+      <BvhPhysicsBody>
+        <PrototypeBox
+          color="#ffffff"
+          scale={[10, 0.5, 10]}
+          position={[0, -2, 0]}
+        />
+
+        {/* Platforms */}
+        <PrototypeBox color="#cccccc" scale={[2, 1, 3]} position={[4, 0, 0]} />
+        <PrototypeBox
+          color="#ffccff"
+          scale={[3, 1, 3]}
+          position={[3, 1.5, -1]}
+        />
+        <PrototypeBox
+          color="#ccffff"
+          scale={[2, 0.5, 3]}
+          position={[2, 2.5, -3]}
+        />
+        <PrototypeBox
+          color="#ffccff"
+          scale={[2, 1, 3]}
+          position={[-3, 0, -2]}
+        />
+        <PrototypeBox color="#ccffff" scale={[1, 1, 4]} position={[0, -1, 0]} />
+        <PrototypeBox
+          color="#ffffcc"
+          scale={[4, 1, 1]}
+          position={[0, 3.5, 0]}
+        />
+      </BvhPhysicsBody>
     </>
   );
 };
