623c171ab300a5fd28b702ba7120b599e1cb1319
tmba <ut42.nu@gmail.com>
2025-08-20 21:03:47 +0900

    MainLevelコンポーネントの照明設定を強化し、Cloudsコンポーネントの位置を調整

--- src/components/MainLevel.tsx
+++ src/components/MainLevel.tsx
@@ -1,9 +1,17 @@
 import { Map } from "@/components/Map";
-import { Sky } from "@react-three/drei";
+import {
+  Cloud,
+  Clouds,
+  Environment,
+  Sky,
+  SoftShadows,
+} from "@react-three/drei";
+import { Bloom, EffectComposer, SMAA, SSAO } from "@react-three/postprocessing";
 import { Physics } from "@react-three/rapier";
 import { BvhPhysicsBody, PrototypeBox } from "@react-three/viverse";
 import { TeleportTarget } from "@react-three/xr";
 import { Vector3 } from "three";
+import * as THREE from "three";
 
 type MainLevelProps = {
   onTeleport?: (position: Vector3) => void;
@@ -12,14 +20,32 @@ type MainLevelProps = {
 export const MainLevel = (props: MainLevelProps) => {
   return (
     <>
+      {/* Image-based lighting for more realistic PBR response */}
+      <Environment preset="sunset" />
       <Sky />
-      <Clouds material={THREE.MeshBasicMaterial} position={[4, 15, -8]}>
-        <Cloud segments={30} bounds={[10, 1, 10]} volume={5} color="white" />
+      <Clouds material={THREE.MeshBasicMaterial} position={[16, 25, -16]}>
+        <Cloud segments={30} bounds={[20, 1, 20]} volume={10} color="white" />
       </Clouds>
 
       {/* Lighting - expanded with shadow settings */}
-      <directionalLight intensity={1.2} position={[5, 10, 10]} castShadow />
-      <ambientLight intensity={1} />
+      <SoftShadows />
+      {/* Main sun light with wide shadow camera to cover large maps */}
+      <directionalLight
+        castShadow
+        position={[50, 80, 50]}
+        intensity={2}
+        shadow-mapSize={[2048, 2048]}
+        shadow-bias={-0.001}
+        shadow-normalBias={0.02}
+        shadow-camera-near={1}
+        shadow-camera-far={400}
+        shadow-camera-top={150}
+        shadow-camera-bottom={-150}
+        shadow-camera-left={-150}
+        shadow-camera-right={150}
+      />
+      {/* Low ambient to keep contrast while avoiding pitch-black areas */}
+      <ambientLight intensity={0.35} />
 
       <TeleportTarget onTeleport={props.onTeleport}>
         <BvhPhysicsBody>
@@ -30,12 +56,20 @@ export const MainLevel = (props: MainLevelProps) => {
           /> */}
           <Physics>
             <Map
-              position={[-3.5, -0.25, -8]}
+              scale={[2, 2, 2]}
+              position={[-2, -0.7, -16]}
               model={"models/city_scene_tokyo.glb"}
             />
           </Physics>
         </BvhPhysicsBody>
       </TeleportTarget>
+
+      {/* Post-processing pipeline: SMAA AA + SSAO + gentle bloom */}
+      {/* <EffectComposer enableNormalPass>
+        <SMAA />
+        <SSAO />
+        <Bloom />
+      </EffectComposer> */}
     </>
   );
 };
