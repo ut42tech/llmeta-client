e8d91e2772c9b6b421ef488c0feb096bd475daa1
tmba <ut42.nu@gmail.com>
2025-08-20 18:34:46 +0900

    MainLevelコンポーネントのPrototypeBoxの設定を整理し、不要なプラットフォームを削除

--- src/components/MainLevel.tsx
+++ src/components/MainLevel.tsx
@@ -19,41 +19,9 @@ export const MainLevel = (props: MainLevelProps) => {
       <TeleportTarget onTeleport={props.onTeleport}>
         <BvhPhysicsBody>
           <PrototypeBox
-            color="#ffffff"
-            scale={[10, 0.5, 10]}
-            position={[0, -2, 0]}
-          />
-
-          {/* Platforms */}
-          <PrototypeBox
-            color="#cccccc"
-            scale={[2, 1, 3]}
-            position={[4, 0, 0]}
-          />
-          <PrototypeBox
-            color="#ffccff"
-            scale={[3, 1, 3]}
-            position={[3, 1.5, -1]}
-          />
-          <PrototypeBox
-            color="#ccffff"
-            scale={[2, 0.5, 3]}
-            position={[2, 2.5, -3]}
-          />
-          <PrototypeBox
-            color="#ffccff"
-            scale={[2, 1, 3]}
-            position={[-3, 0, -2]}
-          />
-          <PrototypeBox
-            color="#ccffff"
-            scale={[1, 1, 4]}
+            color="#fafafa"
+            scale={[10, 1, 10]}
             position={[0, -1, 0]}
-          />
-          <PrototypeBox
-            color="#ffffcc"
-            scale={[4, 1, 1]}
-            position={[0, 3.5, 0]}
           />
         </BvhPhysicsBody>
       </TeleportTarget>
