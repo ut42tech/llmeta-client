679719f052606c6800a0f4da9225c538cada1aaa
tmba <ut42.nu@gmail.com>
2025-08-20 19:35:51 +0900

    Mapコンポーネントを新規作成し、GLTFモデルの読み込みとアニメーション処理を追加

--- src/components/MainLevel.tsx
+++ src/components/MainLevel.tsx
@@ -1,4 +1,6 @@
+import { Map } from "@/components/Map";
 import { Sky } from "@react-three/drei";
+import { Physics } from "@react-three/rapier";
 import { BvhPhysicsBody, PrototypeBox } from "@react-three/viverse";
 import { TeleportTarget } from "@react-three/xr";
 import { Vector3 } from "three";
@@ -23,6 +25,9 @@ export const MainLevel = (props: MainLevelProps) => {
             scale={[10, 1, 10]}
             position={[0, -1, 0]}
           />
+          <Physics>
+            <Map position={[0, 0, 0]} model={"models/city_scene_tokyo.glb"} />
+          </Physics>
         </BvhPhysicsBody>
       </TeleportTarget>
     </>
---
+++ src/components/Map.tsx
@@ -0,0 +1,41 @@
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { RigidBody } from "@react-three/rapier";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+type MapProps = {
+  model: string;
+} & React.JSX.IntrinsicElements["group"];
+
+export const Map = ({ model, ...props }: MapProps) => {
+  const { scene, animations } = useGLTF(model);
+  const group = useRef<THREE.Object3D>(null);
+  const { actions } = useAnimations(animations, group);
+  useEffect(() => {
+    scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+  }, [scene]);
+
+  useEffect(() => {
+    if (
+      actions &&
+      animations.length > 0 &&
+      animations[0]?.name &&
+      actions[animations[0].name]
+    ) {
+      actions[animations[0].name]?.play();
+    }
+  }, [actions, animations]);
+
+  return (
+    <group>
+      <RigidBody type="fixed" colliders="trimesh">
+        <primitive object={scene} {...props} ref={group} />
+      </RigidBody>
+    </group>
+  );
+};
