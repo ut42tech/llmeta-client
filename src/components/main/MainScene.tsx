35a2b0bc2ea41a8a02c7a9180961e43cd9c7bc61
tmba <ut42.nu@gmail.com>
2025-08-21 19:42:52 +0900

    MainSceneコンポーネントから不要なインポートを削除し、コードを簡素化

--- src/components/main/MainScene.tsx
+++ src/components/main/MainScene.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { Level } from "@/components/Level";
-import { PlayerTag } from "@/components/PlayerTag";
-import { SnapRotateXROrigin } from "@/components/xr/SnapRotateXROrigin";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { SimpleCharacter, useXRControllerInput } from "@react-three/viverse";
-import { useCallback, useRef } from "react";
-import { Group, Vector3 } from "three";
 
 export const MainScene = () => {
   return (
