a4bcf19c1b55117ac5a718c2cb0956d2e2ccb700
tmba <ut42.nu@gmail.com>
2025-08-22 16:32:13 +0900

    不要なStatsコンポーネントのコメントアウトを削除

--- src/app/vrm/page.tsx
+++ src/app/vrm/page.tsx
@@ -12,7 +12,6 @@ function VRMPage() {
       <Canvas shadows camera={{ position: [0.25, 0.25, 2], fov: 30 }}>
         <color attach="background" args={["#333"]} />
         <fog attach="fog" args={["#333", 10, 20]} />
-        {/* <Stats /> */}
         <Suspense>
           <VRMScene />
         </Suspense>
