4e53d2c3f71a415c89c000c4f963c9ca6373858b
tmba <ut42.nu@gmail.com>
2025-09-23 01:19:33 +0900

    RemotePlayerコンポーネントからPlayerモデルを削除し、代わりにボックスメッシュを表示するように変更

--- src/components/player/RemotePlayer.tsx
+++ src/components/player/RemotePlayer.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import { Player } from "@/components/player/Player";
 import { PlayerTag } from "@/components/player/PlayerTag";
-
-const MODEL = {
-  type: "vrm",
-  url: "/models/8590256991748008892.vrm",
-  castShadow: true,
-  receiveShadow: true,
-};
 
 type RemotePlayerProps = {
   name: string;
@@ -16,10 +8,13 @@ type RemotePlayerProps = {
 
 export const RemotePlayer = ({ name }: RemotePlayerProps) => {
   return (
-    <>
-      <Player model={MODEL} input={null} cameraBehavior={false}>
-        <PlayerTag name={name} />
-      </Player>
-    </>
+    <group>
+      <mesh castShadow receiveShadow position-y={0.9}>
+        <boxGeometry args={[0.5, 1.8, 0.5]} />
+        <meshStandardMaterial color="#44aaff" />
+      </mesh>
+
+      <PlayerTag name={name} />
+    </group>
   );
 };
