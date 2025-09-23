19bc96c2b7363e654baf9f7713f0b5c80101549e
tmba <ut42.nu@gmail.com>
2025-09-23 19:34:01 +0900

    RemotePlayerコンポーネントのメッシュの位置とサイズを調整し、マテリアルの色と透明度を変更

--- src/components/player/RemotePlayer.tsx
+++ src/components/player/RemotePlayer.tsx
@@ -9,9 +9,9 @@ type RemotePlayerProps = {
 export const RemotePlayer = ({ name }: RemotePlayerProps) => {
   return (
     <group>
-      <mesh castShadow receiveShadow position-y={0.9}>
-        <boxGeometry args={[0.5, 1.8, 0.5]} />
-        <meshStandardMaterial color="#44aaff" />
+      <mesh castShadow receiveShadow position-y={1}>
+        <boxGeometry args={[0.5, 0.5, 0.5]} />
+        <meshStandardMaterial color={"orange"} transparent opacity={0.8} />
       </mesh>
 
       <PlayerTag name={name} />
