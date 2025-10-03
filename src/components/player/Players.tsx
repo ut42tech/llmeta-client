e939848a74944a186a00bd6a17c74647370d600a
tmba <ut42.nu@gmail.com>
2025-10-03 21:28:12 +0900

    fix: build error

--- src/components/player/Players.tsx
+++ src/components/player/Players.tsx
@@ -28,7 +28,7 @@ export const Players = () => {
     if (!state) return list;
 
     // MapSchema の反復取得
-    state.players.forEach((p, id) => {
+    state.players.forEach((p: any, id: string) => {
       if (id === localId) return;
       // サーバーのpositionは「頭（カメラ）」のワールド位置
       const pos: [number, number, number] = [
